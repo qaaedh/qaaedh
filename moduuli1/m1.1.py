384a27cdf2d411d42ce0e191a31d9ca416dd72e3
Kkaae <kkaaed0@gmail.com>
2023-09-05 11:46:56 +0300

    m1-1

---
+++ moduuli1/m1.1.py
@@ -0,0 +1,1 @@
+print('hiPyCharm')
---
+++ moduuli6/6.1.py
@@ -0,0 +1,9 @@
+def tervehdi(kerrat):
+    for i in range(kerrat):
+        print ("Hyvää päivää " + str(i+1) + ". kerran")
+    return
+
+print ("Päivä alkaa tervehdyksillä.")
+tervehdi(5)
+print ("Tervehditään lisää.")
+tervehdi(2)
