e4a6bce82deca1480ab503773cfdaa30503d6b36
Kkaae <kkaaed0@gmail.com>
2024-03-27 14:44:41 +0200

    m

--- m12,1/12-1.py
+++ m12,1/12-1.py
@@ -1,12 +1,19 @@
-
-
-
+import json
 
 import requests
 
-hakusana = input("Anna hakusana: ")
+#hakusana = input("Anna hakusana: ")
+
+hakusana='girls'
 
 # Pyynnön malli: https://api.tvmaze.com/search/shows?q=girls
-pyyntö = "https://api.chucknorris.io/jokes/random" + hakusana
+
+pyyntö = "https://api.tvmaze.com/search/shows?q=" + hakusana
+
+print(pyyntö)
+
 vastaus = requests.get(pyyntö).json()
-print(vastaus)
+
+print(hakusana)
+print(json.dumps(vastaus,indent=2))
+print(vastaus[0]['shwo']['genres'])
