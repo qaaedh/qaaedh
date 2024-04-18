e5c144e45dcb2d788bc2cc4860a6dcdb1fabc169
Kkaae <kkaaed0@gmail.com>
2024-04-18 12:35:19 +0300

    m

--- moduuli13/m13-1.py
+++ moduuli13/m13-1.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 app = Flask(__name__)
+
 def is_prime(luku):
     if luku > 1:
         for i in range(2, int(luku)):
@@ -9,13 +10,13 @@ def is_prime(luku):
     else:
         return False
 
-@app.route('/alkuluku')
-def alkuluku():
-    args=request.args
-    luku = float(args.get("luku"))
+@app.route('/alkuluku/<luku>')
+def alkuluku(luku):
+    luku = float(luku)  # Convert luku to float
     if is_prime(luku):
         return f"{luku} is a prime number."
     else:
         return f"{luku} is not a prime number."
+
 if __name__ == '__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=3000)
